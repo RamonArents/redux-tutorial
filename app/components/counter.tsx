9d260166508c3b161e3d7ed94d32066084c5fb66
RamonArents <ramonarents@hotmail.com>
2025-09-20 15:40:21 +0200

    adde functionality

--- app/components/counter.tsx
+++ app/components/counter.tsx
@@ -1,3 +1,18 @@
+
+import { useDispatch, useSelector } from "react-redux"
+import { decrement, increment, incrementByAmount } from "~/state/counter/counterSlice";
+import type { RootState } from "~/state/store";
+
 export function Counter() {
-    return <p>Tellen</p>
+    const count = useSelector((state:RootState) => state.counter.value);
+    const dispatch = useDispatch();
+
+    return <div>
+        <h2>{count}</h2>
+        <div>
+            <button onClick={() => dispatch(increment())}>Increment</button>
+            <button onClick={() => dispatch(incrementByAmount(10))}>incrementByAmount</button>
+            <button onClick={() => dispatch(decrement())}>Decrement</button>
+        </div>
+    </div>
 }
